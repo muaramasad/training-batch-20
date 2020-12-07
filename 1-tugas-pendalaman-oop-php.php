35a4e8d60e41e0822d0568e934c8a3a51a5a39bd
muaramasad <asadmuharram@gmail.com>
2020-12-08 01:50:41 +0700

    Hari 1 | Tugas Pendalaman OOP PHP

--- 1-tugas-pendalaman-oop-php.php
+++ 1-tugas-pendalaman-oop-php.php
@@ -1,5 +1,96 @@
 <?php
 
-public class Hewan{
+class Hewan {
+    use Fight;
+    public $nama;
+    public $darah = 50;
+    public $jumlahKaki;
+    public $keahlian;
 
+    public function atraksi() {
+        echo $this->nama .' sedang '.$this->keahlian.'<br/>';
+    }
 }
+
+trait Fight {
+    public $attackPower;
+    public $defencePower;
+
+    public function serang(Hewan $lawan) {
+        echo $this->nama.' sedang menyerang '.$lawan->nama.'<br/>';
+        $lawan->diserang($this);
+    }
+
+    public function diserang(Hewan $lawan) {
+        echo $this->nama.' sedang diserang '.$lawan->nama.'<br/>';
+        $this->darah = $this->darah - $lawan->attackPower / $this->defencePower;
+    }
+}
+
+class Elang extends Hewan {
+
+    public $jenisHewan = 'elang';
+
+    public function __construct($nama,$jumlahKaki,$keahlian, $attackPower, $defensePower)
+    {
+        $this->nama = $this->jenisHewan.'_'.$nama;
+        $this->jumlahKaki = $jumlahKaki;
+        $this->keahlian = $keahlian;
+        $this->attackPower = $attackPower;
+        $this->defencePower = $defensePower;
+    }
+
+    public function getInfoHewan() {
+        echo 'Nama: '.$this->nama.'<br/>';
+        echo 'Jenis Hewan: '.$this->jenisHewan.'<br/>';
+        echo 'Attack Power: '.$this->attackPower.'<br/>';
+        echo 'Defence Power: '.$this->defencePower.'<br/>';
+        echo 'Darah: '.$this->darah.'<br/>';
+        echo 'Jumlah Kaki: '.$this->jumlahKaki.'<br/>';
+        echo 'Keahlian: '.$this->keahlian.'<br/>';
+
+    }
+}
+
+class Harimau extends Hewan {
+
+    public $jenisHewan = 'harimau';
+
+    public function __construct($nama,$jumlahKaki,$keahlian, $attackPower, $defensePower)
+    {
+        $this->nama = $this->jenisHewan.'_'.$nama;
+        $this->jumlahKaki = $jumlahKaki;
+        $this->keahlian = $keahlian;
+        $this->attackPower = $attackPower;
+        $this->defencePower = $defensePower;
+    }
+
+    public function getInfoHewan() {
+        echo 'Nama: '.$this->nama.'<br/>';
+        echo 'Jenis Hewan: '.$this->jenisHewan.'<br/>';
+        echo 'Attack Power: '.$this->attackPower.'<br/>';
+        echo 'Defence Power: '.$this->defencePower.'<br/>';
+        echo 'Darah: '.$this->darah.'<br/>';
+        echo 'Jumlah Kaki: '.$this->jumlahKaki.'<br/>';
+        echo 'Keahlian: '.$this->keahlian.'<br/>';
+    }
+}
+
+$elang = new Elang('2',2,'terbang tinggi', 10, 5 );
+$harimau = new Harimau('5',4,'lari cepat', 7, 8 );
+
+$elang->getInfoHewan();
+echo "<br/>";
+$harimau->getInfoHewan();
+echo "<br/>";
+$elang->atraksi();
+$harimau->atraksi();
+echo "<br/>";
+$elang->serang($harimau);
+echo "<br/>";
+$harimau->serang($elang);
+echo "<br/>";
+$elang->getInfoHewan();
+echo "<br/>";
+$harimau->getInfoHewan();
+echo "<br/>";
