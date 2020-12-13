d59c8c43b726ba23a97888fa2bb745fc7186245e
muaramasad <asadmuharram@gmail.com>
2020-12-14 03:19:17 +0700

    add verification API endpoint | Materi Rest API

---
+++ app-crowdfunding/app/Http/Controllers/Auth/VerificationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\OtpCode;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class VerificationController extends AuthController
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $currentTiming = Carbon::now();
+        $otpCode = OtpCode::where('code',$request->otp)->first();
+
+        if(!$otpCode){
+            $response = [
+                "response_code" => $this->failCode,
+                "response_message" => $this->otpNotFound
+            ];
+            return response($response, 200)
+                ->header('Content-Type', 'text/json');
+        }
+
+        if($otpCode->expired > $currentTiming){
+            $response = [
+                "response_code" => $this->failCode,
+                "response_message" => $this->otpExpired
+            ];
+            return response($response, 200)
+                ->header('Content-Type', 'text/json');
+        }
+
+        $user = User::find($otpCode->user_id);
+        $user->email_verified_at = $currentTiming;
+        $user->save();
+
+        $otpCode->delete();
+        $data['user'] = $user;
+
+        $response = [
+            "response_code" => $this->successCode,
+            "response_message" => "silahkan cek email",
+            "data" => $data
+        ];
+
+        return response($response, 200)
+            ->header('Content-Type', 'text/json');
+    }
+}
--- app-crowdfunding/routes/api.php
+++ app-crowdfunding/routes/api.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::post('register','Auth\RegisterController');
+Route::post('verification','Auth\VerificationController');
