e92dc264627a16a17dcf41de348638681c327fed
muaramasad <asadmuharram@gmail.com>
2020-12-14 01:53:51 +0700

    add nullable for photo path & password | Materi Rest API

--- app-crowdfunding/database/migrations/2014_10_12_000000_create_users_table.php
+++ app-crowdfunding/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->text('photo_path');
+            $table->text('photo_path')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();;
             $table->rememberToken();
             $table->timestamps();
         });
