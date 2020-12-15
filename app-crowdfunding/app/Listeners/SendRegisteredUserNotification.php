0929e344754b88db9287fcdec8d9942e9e692fc0
muaramasad <asadmuharram@gmail.com>
2020-12-16 02:33:00 +0700

    add queue | Materi Event, Listener & Queue
    implement a queue for sending an email when user register their account

--- app-crowdfunding/app/Listeners/SendRegisteredUserNotification.php
+++ app-crowdfunding/app/Listeners/SendRegisteredUserNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendRegisteredUserNotification
+class SendRegisteredUserNotification implements ShouldQueue
 {
     /**
      * Create the event listener.
