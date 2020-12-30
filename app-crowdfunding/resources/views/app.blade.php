b527093e75f6d89fdb864daaaa9e59852464bb30
muaramasad <asadmuharram@gmail.com>
2020-12-31 02:10:26 +0700

    add vue js

---
+++ app-crowdfunding/resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="{{ url('/css/app.css') }}">
+    </head>
+    <body>
+        <h1>Sanbercode</h1>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script src="{{ url('/js/app.js') }}"></script>
+    </body>
+</html>
