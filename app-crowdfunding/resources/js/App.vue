1b16be7c96f57003af8b44cc3a3ba56c74bb1546
muaramasad <asadmuharram@gmail.com>
2021-01-03 05:19:17 +0700

    update header

--- app-crowdfunding/resources/js/App.vue
+++ app-crowdfunding/resources/js/App.vue
@@ -52,7 +52,7 @@
 
         </v-navigation-drawer>
 
-        <v-app-bar app color="success" dark>
+        <v-app-bar app color="success" dark v-if="isHome">
             <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
             <v-toolbar-title>SanbercodeApp</v-toolbar-title>
 
@@ -76,6 +76,21 @@
                 prepend-inner-icon="mdi-magnify"
                 solo-inverted
             ></v-text-field>
+        </v-app-bar>
+
+        <v-app-bar app color="success" dark v-else>
+            <v-btn icon @click.stop="$router.go(-1)">
+                <v-icon>mdi-arrow-left-circle</v-icon>
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-badge color="orange" overlap>
+                    <template v-slot:badge>
+                        <span>3</span>
+                    </template>
+                    <v-icon>mdi-cash-multiple</v-icon>
+                </v-badge>
+            </v-btn>
         </v-app-bar>
 
         <!-- Sizes your content based upon application components -->
@@ -109,6 +124,11 @@
                 { title: 'Campaigns', icon: 'mdi-hand-heart', route: '/campaigns'}
             ],
             guest: false
-        })
+        }),
+        computed: {
+            isHome() {
+                return (this.$route.path === '/' || this.$route.path === '/home')
+            }
+        }
     }
 </script>
