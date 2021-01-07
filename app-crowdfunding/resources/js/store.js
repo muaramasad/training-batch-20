8c7f11df2934aad5b8fd8340d59a2e86394bd6f6
muaramasad <asadmuharram@gmail.com>
2021-01-08 04:19:57 +0700

    fix missing token when page refreshed using vuex persist

--- app-crowdfunding/resources/js/store.js
+++ app-crowdfunding/resources/js/store.js
@@ -5,10 +5,17 @@ import transaction from "./stores/transaction";
 import alert from "./stores/alert";
 import auth from "./stores/auth";
 import dialog from "./stores/dialog";
+import VuexPersist from 'vuex-persist';
+
+const vuexPersist = new VuexPersist({
+    key: 'crowdfunding',
+    storage: localStorage
+});
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+    plugins: [vuexPersist.plugin],
     modules: {
         transaction,
         alert,
